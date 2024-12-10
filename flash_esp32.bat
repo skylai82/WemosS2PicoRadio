3d3cdc7e1b879767dddc897ddf14982609e4b3f3
skylai82 <37041202+skylai82@users.noreply.github.com>
2024-12-10 11:31:46 +0800

    Add files via upload

---
+++ flash_esp32.bat
@@ -0,0 +1,6 @@
+@echo off
+python -m esptool --chip esp32 --port COM3 --baud 921600 write_flash ^
+0x1000 CudeRadioPlaylist.ino.bootloader.bin ^
+0x8000 CudeRadioPlaylist.ino.partitions.bin ^
+0x10000 CudeRadioPlaylist.ino.bin
+pause
